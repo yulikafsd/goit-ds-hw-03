4e1a03fe73f5a01192c860d967281faee5f47ff7
Julia Zahorovska <ju.zagorovsky@gmail.com>
2024-06-19 16:44:21 +0200

    fix_seed

--- seed.py
+++ seed.py
@@ -1,27 +1,57 @@
-from random import randint
+from random import randint, sample
 from faker import Faker
 from client import db
 
-fake = Faker(["it_IT", "es_ES", "en_US"])
+fake = Faker(["uk_UA", "it_IT", "es_ES", "en_US"])
+
+cat_features = [
+    "хижий",
+    "гуляє сам по собі",
+    "граційний",
+    "витончений",
+    "робить те, що хоче",
+    "любить свободу",
+    "прив’язується до господаря",
+    "чутливий",
+    "відчуває настрій",
+    "грайливий",
+    "спокійний",
+    "витримує обійми",
+    "галасує ночами",
+    "вередливий",
+    "пухнастий",
+    "не любить гратись",
+    "любить дітей",
+    "бойовий",
+    "розумний",
+    "великий",
+    "добродушний",
+    "має милі складочки",
+    "має великі вушка",
+    "ходить на лоток",
+    "чухається",
+    "дере диван",
+]
+
 
 cats_list = []
 
 
 def generate_features():
-    features_list = []
-    for _ in range(randint(1, 5)):
-        features_list.append(" ".join(fake.words(randint(1, 4))))
+    features_list = sample(cat_features, randint(1, 5))
     return features_list
 
 
-for _ in range(10):
-    cats_list.append(
-        {
-            "name": fake.first_name(),
-            "age": randint(0, 15),
-            "features": generate_features(),
-        }
-    )
+def create_cats(num):
+    for _ in range(num):
+        cats_list.append(
+            {
+                "name": fake.first_name(),
+                "age": randint(0, 15),
+                "features": generate_features(),
+            }
+        )
+    return cats_list
 
 
-db.cats.insert_many(cats_list)
+db.cats.insert_many(create_cats(10))
